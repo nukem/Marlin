2a113fd5e81075885157644d0095b8fc8aecad7c
Francis V <lugaw@outlook.com>
2018-09-14 20:04:29 +0900

    Fix the xy jerk

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -644,8 +644,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                  5.0
-#define DEFAULT_YJERK                  5.0
+#define DEFAULT_XJERK                  10.0
+#define DEFAULT_YJERK                  10.0
 #define DEFAULT_ZJERK                  0.3
 #define DEFAULT_EJERK                  4.0
 
